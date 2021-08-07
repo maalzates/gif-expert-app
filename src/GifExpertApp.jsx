643c3b869a4f98a405fd6f3dcfd9b2319945ef36
Manuel Alzate <maalzates@gmail.com>
2021-08-07 11:31:54 -0300

    Initiated first component `

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const GifExpertApp = () => {
+    const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+
+
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <hr />
+
+            {
+                categories
+            }
+        </>
+    )
+}
+
+export default GifExpertApp;
