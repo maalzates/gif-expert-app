cba0cbd25f2ef65d3fe7fa620b10a25c0f45322a
Manuel Alzate <maalzates@gmail.com>
2021-08-08 23:40:11 -0300

    Rendering Rick and Morty gifs, adding styles

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
+import AddCategory from './components/AddCategory';
+import GifGrid from './components/GifGrid';
+
 
 const GifExpertApp = () => {
-    const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+    const [categories, setCategories] = useState(['One Punch'])
 
-
+    // const handleAdd = () => {
+    //     setCategories(categories => [...categories, 'Dog'])
+    // }
 
     return (
         <>
             <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />
             <hr />
 
-            {
-                categories
-            }
+            <ol>
+                {
+                    categories.map(category =>
+                        <GifGrid
+                            key={category}
+                            category={category} />
+                    )
+                }
+            </ol>
         </>
     )
 }
